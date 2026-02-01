8c54273dbb79c3a759e4d0d804b046e3779b8713
mahmoud ahmed <mahmoudahmedhsn@gmail>
2026-02-01 21:00:03 +0200

    add flutter svg and gap package for using in project

--- lib/core/utils/api_util.dart
+++ lib/core/utils/api_util.dart
@@ -14,7 +14,7 @@ class ApiUtils {
       getToken: () => getIt.get<AppStorage>().getToken(),
       interceptors: [RequestsInspectorInterceptor()],
       tokenType: ApiRequestOptions.bearer,
-      enableLog: true,
+      logLevel: ApiLogLevel.debug,
       onError: (error) {
         NotificationUtil.showError(error.message);
       },
