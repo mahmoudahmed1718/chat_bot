b1406a166541bf8596b0479836e7d95a8280e0c9
mahmoud ahmed <mahmoudahmedhsn@gmail>
2026-02-01 21:04:59 +0200

    create Assets file for image by flutter assets extension

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesOnBordingImage
+  /// assets/images/on_bording_image.svg
+  static const String assetsImagesOnBordingImage = "assets/images/on_bording_image.svg";
+
+  /// Assets for assetsImagesRebotImage
+  /// assets/images/rebot_image.svg
+  static const String assetsImagesRebotImage = "assets/images/rebot_image.svg";
+
+  /// Assets for assetsImagesSplashImage
+  /// assets/images/splash_image.svg
+  static const String assetsImagesSplashImage = "assets/images/splash_image.svg";
+}
+
