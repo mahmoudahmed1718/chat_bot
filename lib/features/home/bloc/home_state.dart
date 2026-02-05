519c6d035412873a43db77cf979c2768cc0bd73f
mahmoud ahmed <mahmoudahmedhsn@gmail>
2026-02-05 02:33:55 +0200

    implement code  in home state

--- lib/features/home/bloc/home_state.dart
+++ lib/features/home/bloc/home_state.dart
@@ -1,10 +1,21 @@
+import 'package:chat_bot/app/data/gemine_model.dart';
 import 'package:equatable/equatable.dart';
 
 class HomeState extends Equatable {
-  const HomeState();
-
-  HomeState copyWith() => HomeState();
+  const HomeState({this.error, this.isLoading, this.geminiModel});
+  final String? error;
+  final bool? isLoading;
+  final GeminiModel? geminiModel;
+  HomeState copyWith(
+    String? error,
+    bool? isLoading,
+    GeminiModel? geminiModel,
+  ) => HomeState(
+    error: error ?? this.error,
+    isLoading: isLoading ?? this.isLoading,
+    geminiModel: geminiModel ?? this.geminiModel,
+  );
 
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => [error, isLoading, geminiModel];
 }
